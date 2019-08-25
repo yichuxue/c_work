a99c402ba399f9ecbc7d9631a03f4237a641544a
lex <944301165@qq.com>
2019-08-25 21:31:10 +0800

    add

--- aa.c
+++ aa.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include <windows.h>
 
+
 void main()
-{
+{   
+    void csd();
     int a, b, c, d;
     unsigned u;
     a=12;b=24;u=10;
     c = a + u;
     d = b + u;
-    printf("a+u=%d, b+u=%d\n", c, d);
-    system("pause");
+    // printf("a+u=%d, b+u=%d\n", c, d);
+    // system("pause");
+    csd();
+}
+
+void csd()
+{
+    char c;
+    printf("input a character\n");
+    c = getchar();
+    putchar(c);
 }
