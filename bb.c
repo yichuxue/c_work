1865eb460bb5e2d1f367dd6a1079853eebde0263
lex <944301165@qq.com>
2019-08-25 21:47:09 +0800

    add mas function

--- bb.c
+++ bb.c
@@ -2,6 +2,14 @@
 #include <windows.h>
 
 void main() {
+    int mas(int x, int y);
     printf("bb.c\n");
     system("pause");
+}
+
+int mas(int x, int y)
+{
+    int z;
+    z = z > y ? z : y;
+    return z;
 }
